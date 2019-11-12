99d2b33fd3335cc6a08d06b8b820105eae6f2aa5
Tetiana <tanichk@ukr.net>
2019-11-12 16:59:02 +0200

    add select

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -7,10 +7,34 @@ const List = (props) => {
         users: {},
         isFetching: false
     });
+
     const [product, setProduct] = useState({
         title: '',
         description: ''
     });
+
+    const [select, setSelect] = useState({
+        options: [
+            {
+                value: 'forklift',
+                label: 'Forklift'
+            }, 
+            {
+                value: 'spare parts',
+                label: 'Spare Parts'
+            }
+        ],
+        chooseValue: ''
+    })
+
+    const handleChangeSelect = e => {
+        e.preventDefault();
+        setSelect({
+            ...select,
+            chooseValue: e.target.value
+        })
+    }
+    console.log(select);
 
     const handleChangeInput = event => {
         event.preventDefault();
@@ -26,12 +50,12 @@ const List = (props) => {
                     users: data.users,
                     isFetching: true
                 })
-                const responce = await axios.get('https://forklift-bb1ea.firebaseio.com/items.json');
+                const response = await axios.get('https://forklift-bb1ea.firebaseio.com/items.json');
                 setData({
-                    users: responce.data,
+                    users: response.data,
                     isFetching: false
                 })
-                console.log(responce.data);
+                console.log(response.data);
             }
             catch (e) {
                 console.log(e);
@@ -42,114 +66,141 @@ const List = (props) => {
             }
         }
         fetchUsers();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <div className="container">
             <div className="Items-list">
+
+                <div className="form-group">
+                    <label className="col-form-label">Choose type of product:</label>
+                    <select className="form-control" onChange={handleChangeSelect}>
+                        {
+                            select.options.map((item, key) => (
+                                <option value={item.value} key={key}>
+                                    {item.label}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </div>
+
                 {
                     !props.currentUser ? (
-                        <div>
+                        <div className={`${select.chooseValue === 'forklift' ? 'd-block' : 'd-none'}`}>
                             {/* <p>You are logged in as {props.currentUser.user.email}. Now you can add some products of this list!</p> */}
                             <form>
-                                <div className="form-group row">
-                                    <label htmlFor="title" className="col-md-3">Name</label>
-                                    <input 
-                                        type="text"
-                                        placeholder="Enter Name"
-                                        className="form-control col-md-9"
-                                        name="title"
-                                        value={product.title}
-                                        onChange={handleChangeInput} />
+                                <div className="form-row">
+                                    <div className="form-group col-md-6">
+                                        <label htmlFor="title">Model:</label>
+                                        <input 
+                                            type="text"
+                                            placeholder="Enter Model"
+                                            className="form-control"
+                                            name="model"
+                                            value={product.title}
+                                            onChange={handleChangeInput} />
+                                    </div>
+                                    <div className="form-group col-md-6">
+                                        <label htmlFor="title">Capacity, kg:</label>
+                                        <input 
+                                            type="text"
+                                            placeholder="Enter Capacity"
+                                            className="form-control"
+                                            name="capacity"
+                                            value={product.title}
+                                            onChange={handleChangeInput} />
+                                    </div>
                                 </div>
-                                <div className="form-group row">
-                                    <label htmlFor="title" className="col-md-3">Вантажопідйомність, кг</label>
-                                    <input 
-                                        type="text"
-                                        placeholder="Enter Name"
-                                        className="form-control col-md-9"
-                                        name="title"
-                                        value={product.title}
-                                        onChange={handleChangeInput} />
+
+                                <div className="form-row">
+                                    <div className="form-group col-md-6">
+                                        <label htmlFor="title">Power Type:</label>
+                                        <input 
+                                            type="text"
+                                            placeholder="Power Type"
+                                            className="form-control"
+                                            name="power"
+                                            value={product.title}
+                                            onChange={handleChangeInput} />
+                                    </div>
+                                    <div className="form-group col-md-6">
+                                        <label htmlFor="title">Type of transmision:</label>
+                                        <input 
+                                            type="text"
+                                            placeholder="Type of transmision"
+                                            className="form-control"
+                                            name="transmision"
+                                            value={product.title}
+                                            onChange={handleChangeInput} />
+                                    </div>
                                 </div>
-                                <div className="form-group row">
-                                    <label htmlFor="title" className="col-md-3">Тип двигуна</label>
-                                    <input 
-                                        type="text"
-                                        placeholder="Enter Name"
-                                        className="form-control col-md-9"
-                                        name="title"
-                                        value={product.title}
-                                        onChange={handleChangeInput} />
+
+                                <div className="form-row">                                
+                                    <div className="form-group col-md-6">
+                                        <label htmlFor="title">Lift height, mm:</label>
+                                        <input 
+                                            type="text"
+                                            placeholder="Lift height"
+                                            className="form-control"
+                                            name="lift_height"
+                                            value={product.title}
+                                            onChange={handleChangeInput} />
+                                    </div>
+                                    <div className="form-group col-md-6">
+                                        <label htmlFor="title">Free lift (+/-), mm:</label>
+                                        <input 
+                                            type="text"
+                                            placeholder="Free lift"
+                                            className="form-control"
+                                            name="free_lift"
+                                            value={product.title}
+                                            onChange={handleChangeInput} />
+                                    </div>
                                 </div>
-                                <div className="form-group row">
-                                    <label htmlFor="title" className="col-md-3">Тип трансмісії</label>
-                                    <input 
-                                        type="text"
-                                        placeholder="Enter Name"
-                                        className="form-control col-md-9"
-                                        name="title"
-                                        value={product.title}
-                                        onChange={handleChangeInput} />
+
+                                <div className="form-row">
+                                    <div className="form-group col-md-6">
+                                        <label htmlFor="title">Tyres type:</label>
+                                        <input 
+                                            type="text"
+                                            placeholder="Tyres type"
+                                            className="form-control"
+                                            name="tyres"
+                                            value={product.title}
+                                            onChange={handleChangeInput} />
+                                    </div>
+                                    <div className="form-group col-md-6">
+                                        <label htmlFor="title">Fork, mm:</label>
+                                        <input 
+                                            type="text"
+                                            placeholder="Fork"
+                                            className="form-control"
+                                            name="fork"
+                                            value={product.title}
+                                            onChange={handleChangeInput} />
+                                    </div>
                                 </div>
-                                <div className="form-group row">
-                                    <label htmlFor="title" className="col-md-3">Висота підйому, мм</label>
-                                    <input 
-                                        type="text"
-                                        placeholder="Enter Name"
-                                        className="form-control col-md-9"
-                                        name="title"
-                                        value={product.title}
-                                        onChange={handleChangeInput} />
-                                </div>
-                                <div className="form-group row">
-                                    <label htmlFor="title" className="col-md-3">Вільний хід (+/-), мм</label>
-                                    <input 
-                                        type="text"
-                                        placeholder="Enter Name"
-                                        className="form-control col-md-9"
-                                        name="title"
-                                        value={product.title}
-                                        onChange={handleChangeInput} />
-                                </div>
-                                <div className="form-group row">
-                                    <label htmlFor="title" className="col-md-3">Тип шин</label>
-                                    <input 
-                                        type="text"
-                                        placeholder="Enter Name"
-                                        className="form-control col-md-9"
-                                        name="title"
-                                        value={product.title}
-                                        onChange={handleChangeInput} />
-                                </div>
-                                <div className="form-group row">
-                                    <label htmlFor="title" className="col-md-3">Довжина вил, мм</label>
-                                    <input 
-                                        type="text"
-                                        placeholder="Enter Name"
-                                        className="form-control col-md-9"
-                                        name="title"
-                                        value={product.title}
-                                        onChange={handleChangeInput} />
-                                </div>
-                                <div className="form-group row">
-                                    <label htmlFor="description" className="col-md-3">Description</label>
+
+                                <div className="form-group">
+                                    <label htmlFor="description">Detail information:</label>
                                     <textarea 
                                         type="text"
-                                        placeholder="Enter Description"
-                                        className="form-control col-md-9"
+                                        placeholder="Detail information"
+                                        className="form-control"
                                         name="description"
                                         value={product.description}
                                         onChange={handleChangeInput}>
                                     </textarea>
                                 </div>
-                                <button
-                                    style={{'marginBottom': '50px'}}
-                                    type="button"
-                                    className="btn btn-primary">
-                                    Add product
-                                </button>
+
+                                <div className="d-flex justify-content-start">
+                                    <button
+                                        type="button"
+                                        className="btn btn-primary">
+                                        Add product
+                                    </button>
+                                </div>
                             </form>
                         </div>
                     ) : (
@@ -157,7 +208,7 @@ const List = (props) => {
                     )
                 }
 
-                <h2 className="title text-center">Навантажувачі власного виробництва</h2>
+                <h2 className="title text-center" style={{marginTop: '50px', marginBottom: '30px'}}>Forklifts of own production</h2>
 
                 <ul>
                     {
