5bab34e8e2299e9ac5829bb0784b8f85447a971d
Tetiana <tanichk@ukr.net>
2019-11-28 16:01:48 +0200

    delete import axios

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 import { myFirebase } from '../../firebase';
 
 const List = (props) => {
